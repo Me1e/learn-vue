62d177e5dc84825ce7eccad81fde634d820f9320
me1e <mele0404@naver.com>
2022-07-03 21:36:17 +0900

    setting eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,24 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
-}
+    "@vue/eslint-config-prettier",
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        semi: true,
+        tabWidth: 2,
+        trailingComma: "all",
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+      },
+    ],
+  },
+};
